4b5a222f9e0bcb9094f0cbebf3a443cfd8d8795c
Alexander Sakhaev <alsakhaev@gmail.com>
2023-07-27 09:26:29 +0300

    feat: cross-domain scenario redirects

--- packages/main/src/App.tsx
+++ packages/main/src/App.tsx
@@ -1,23 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+// ToDo: make recursive with array of urls
+function createCallbackUrl(baseUrl: string, callbackUrl: string): string {
+  const url = new URL(baseUrl);
+  url.searchParams.append("callback", callbackUrl);
+  return url.href;
+}
 
 function App() {
+  const handleScenarioOne = () => {
+    const scenarioOneUrl = process.env.REACT_APP_DOMAIN_SCENARIO_ONE || "";
+    window.location.href = createCallbackUrl(scenarioOneUrl, window.location.href);
+  };
+
+  const handleScenarioTwo = () => {
+    const scenarioTwoUrl = process.env.REACT_APP_DOMAIN_SCENARIO_TWO || "";
+    window.location.href = createCallbackUrl(scenarioTwoUrl, window.location.href);
+  };
+
+  const handleScenarioOneTwo = () => {
+    const scenarioOneUrl = process.env.REACT_APP_DOMAIN_SCENARIO_ONE || "";
+    const scenarioTwoUrl = process.env.REACT_APP_DOMAIN_SCENARIO_TWO || "";
+    window.location.href = createCallbackUrl(scenarioOneUrl, createCallbackUrl(scenarioTwoUrl, window.location.href));
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          <button onClick={handleScenarioOne}>Scenario 1</button>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+          <button onClick={handleScenarioTwo}>Scenario 2</button>
+        </p>
+        <p>
+          <button onClick={handleScenarioOneTwo}>Scenario 1 + 2</button>
+        </p>
       </header>
     </div>
   );
--- packages/scenario-one/src/App.tsx
+++ packages/scenario-one/src/App.tsx
@@ -1,23 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const handleNext = () => {
+    const url = new URL(window.location.href);
+    const callbackUrl = url.searchParams.get("callback") ?? "";
+    window.location.href = callbackUrl;
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <p>Scenario One</p>
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          <button onClick={handleNext}>Next</button>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
