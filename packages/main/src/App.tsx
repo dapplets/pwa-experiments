511cbf4ca5be6164852968e423e6d17ddc178be9
Alexander Sakhaev <alsakhaev@gmail.com>
2023-07-27 09:42:23 +0300

    feat: add texts

--- packages/main/src/App.tsx
+++ packages/main/src/App.tsx
@@ -12,32 +12,51 @@ function createCallbackUrl(baseUrl: string, callbackUrl: string): string {
 function App() {
   const handleScenarioOne = () => {
     const scenarioOneUrl = process.env.REACT_APP_DOMAIN_SCENARIO_ONE || "";
-    window.location.href = createCallbackUrl(scenarioOneUrl, window.location.href);
+    window.location.href = createCallbackUrl(
+      scenarioOneUrl,
+      window.location.href
+    );
   };
 
   const handleScenarioTwo = () => {
     const scenarioTwoUrl = process.env.REACT_APP_DOMAIN_SCENARIO_TWO || "";
-    window.location.href = createCallbackUrl(scenarioTwoUrl, window.location.href);
+    window.location.href = createCallbackUrl(
+      scenarioTwoUrl,
+      window.location.href
+    );
   };
 
   const handleScenarioOneTwo = () => {
     const scenarioOneUrl = process.env.REACT_APP_DOMAIN_SCENARIO_ONE || "";
     const scenarioTwoUrl = process.env.REACT_APP_DOMAIN_SCENARIO_TWO || "";
-    window.location.href = createCallbackUrl(scenarioOneUrl, createCallbackUrl(scenarioTwoUrl, window.location.href));
+    window.location.href = createCallbackUrl(
+      scenarioOneUrl,
+      createCallbackUrl(scenarioTwoUrl, window.location.href)
+    );
   };
 
   return (
     <div className="App">
       <header className="App-header">
-        <p>
+        <div>
+          <p>
+            Should redirect to the 1st scenario, then return to the main page
+          </p>
           <button onClick={handleScenarioOne}>Scenario 1</button>
-        </p>
-        <p>
+        </div>
+        <div>
+          <p>
+            Should redirect to the 2nd scenario, then return to the main page
+          </p>
           <button onClick={handleScenarioTwo}>Scenario 2</button>
-        </p>
-        <p>
+        </div>
+        <div>
+          <p>
+            Should redirect to the 1st scenario, then to the 2nd, then return to
+            the main page
+          </p>
           <button onClick={handleScenarioOneTwo}>Scenario 1 + 2</button>
-        </p>
+        </div>
       </header>
     </div>
   );
